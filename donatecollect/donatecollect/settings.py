39f344122b8219ee838ebd5da3067d614944c39d
Valerii <spaceocean@outlook.com>
2025-04-25 14:03:17 +0500

    .env added

--- donatecollect/donatecollect/settings.py
+++ donatecollect/donatecollect/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xo25h(x(7j$$ddy(19$3vc%@do1ju1puc)p!56as-v47tjza_z'
@@ -107,6 +110,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'Spaceocean21@yandex.ru'
-EMAIL_HOST_PASSWORD = 'aqdufliriwskyfjb'
-DEFAULT_FROM_EMAIL = 'Spaceocean21@yandex.ru'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
